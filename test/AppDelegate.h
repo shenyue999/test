aeb83083b7bfb8470053196249383edd1343a5fe
mlxing <sheny@mlxing.com>
2017-03-27 10:27:55 +0800

    xxx

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by mlxing on 17/3/27.
+//  Copyright © 2017年 mlxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by mlxing on 17/3/27.
+//  Copyright © 2017年 mlxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
